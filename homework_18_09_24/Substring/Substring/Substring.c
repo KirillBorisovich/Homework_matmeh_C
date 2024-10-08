9d4996527a6efde802f79c2f81d1638178227aad
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-10-08 17:07:01 +0300

    fix: corrected the task with the substring

--- homework_18_09_24/Substring/Substring/Substring.c
+++ homework_18_09_24/Substring/Substring/Substring.c
@@ -5,24 +5,27 @@
 #include <stdlib.h>
 #include <locale.h>
 
+bool stringMatching(const char* string, const char* substringint, int substringLen, int i) {
+    for (int j = 0; j < substringLen; ++j) {
+        if (string[i + j] != substringint[j]) {
+            return false;
+            printf("%c%c ", string[i], string[i + 1]);
+        }
+    }
+    return true;
+}
 int countString(const char* string, const char* substring) {
     int stringLen = strlen(string);
     const int substringLen = strlen(substring);
     int counter = 0;
-    for (int i = 0; i < stringLen - substringLen; ++i) {
-        char* sliseString = (char*)calloc(substringLen + 1, sizeof(char));
-        for (int j = 0; j < substringLen; ++j) {
-            sliseString[j] = string[i + j];
-        }
-        if (strcmp(sliseString, substring) == '\0') {
+    for (int i = 0; i < stringLen - substringLen + 1; ++i) {
+        if (stringMatching(string, substring, substringLen, i)) {
             ++counter;
         }
-        free(sliseString);
     }
-
     return counter;
 }
 
 int main(void) {
-    printf("\nSubstring:\n number of substrings: %d\n----------", countString("qwqwqwqw", "qw"));
+    printf("\nSubstring:\n number of substrings: %d\n----------", countString("qwqwqw", "wq"));
 }
