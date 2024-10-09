82f31f0d77ed56bf984754dec5f73212aa998c82
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-10-09 14:18:24 +0300

    feat: completed the first task

---
+++ test_09_10_24/task1/task1/task1.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <locale.h>
+
+bool palindrome(const char* string) {
+	int stringLen = strlen(string);
+	for (int i = 0; i < stringLen / 2; ++i) {
+		if ((string[i] != string[stringLen - i - 1])) {
+			return false;
+		}
+	}
+		return true;
+}
+
+bool testPalindrome() {
+	return palindrome("БУБ") && palindrome("BUUB") && palindrome("буб")
+		&& !palindrome("БУЛБ") && !palindrome("BUK") && palindrome("buub");
+}
+
+int main(void) {
+	setlocale(LC_ALL, "Rus");
+	if (!testPalindrome()) {
+		printf("Test failed");
+		return -1;
+	}
+	char * string = "BUUIB";
+	printf("%d", palindrome(string));
+}
