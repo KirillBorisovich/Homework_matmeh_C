070692592581a27bc94fb3719685a684ddb8c28d
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-10-09 15:13:07 +0300

    feat: completed the second task

---
+++ test_09_10_24/task2/task2/task2.c
@@ -0,0 +1,54 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <locale.h>
+
+void printArray(int array[], int lengh) {
+    for (int i = 0; i < lengh; ++i) {
+        printf("%d ", array[i]);
+    }
+}
+bool arrayComparison(int array1[], int array2[], int lenghs) {
+    for (int i = 0; i < lenghs; ++i) {
+        if (array1[i] != array2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void shellSorting(int array[], int lengh) {
+    for (int i = lengh / 2; i > 0; i /= 2) {
+        for (int k = i; k < lengh; ++k) {
+            for (int j = k - i; j >= 0 && array[j] > array[j + i]; j -= i) {
+                int temporaryVariable = array[j];
+                array[j] = array[j + i];
+                array[j + i] = temporaryVariable;
+            }
+        }
+    }
+}
+
+
+bool testShellSorting() {
+    int array[] = { 1, 3, 5, 8, 6, -2 };
+    int sortedArray[] = { -2, 1, 3, 5, 6, 8 };
+    shellSorting(array, 6);
+    return arrayComparison(array, sortedArray, 6);
+}
+
+int main(void) {
+    setlocale(LC_ALL, "Rus");
+    if (!testShellSorting()) {
+        printf("Test failed");
+        return 1;
+    }
+    int array[] = { 5, 2, 3, 1, 4 };
+    printf("Не отсортированный массив: ");
+    printArray(array, 5);
+    shellSorting(array, 5);
+    printf("\nОтсортированный массив: ");
+    printArray(array, 5);
+
+
+
+}
