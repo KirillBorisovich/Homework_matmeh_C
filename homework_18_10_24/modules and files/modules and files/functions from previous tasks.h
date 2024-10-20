33306f3a8b5127745f6f0ac7cee50128797016cc
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-10-20 15:35:59 +0300

    init: did the assignment with modules

---
+++ homework_18_10_24/modules and files/modules and files/functions from previous tasks.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void swap(int* left, int* right);
+
+void printArray(int array[], int lengh);
+
+void fillArray(int array[], int lengh);
+
+bool arrayComparison(int array1[], int array2[], int lenghs);
---
+++ homework_18_10_24/modules and files/modules and files/sorting.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void insertSort(int array[], int first, int last);
+
+void smartQSort(int array[], int first, int last);
