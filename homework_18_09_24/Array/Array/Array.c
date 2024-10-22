e79404b4882aa15065249695741890ed5080bf7c
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-10-22 20:36:16 +0300

    refactor: reworked the code according to the instructions

--- homework_18_09_24/Array/Array/Array.c
+++ homework_18_09_24/Array/Array/Array.c
@@ -1,9 +1,4 @@
 ﻿#include <stdio.h>
-#include <stdbool.h>
-#include <math.h>
-#include <string.h>
-#include <stdlib.h>
-#include <locale.h>
 
 void swap(int* left, int* right) {
     if (left != right) {
@@ -13,38 +8,23 @@ void swap(int* left, int* right) {
     }
 }
 
-int lenghArray(int array[]) {
-    int counter = 0;
-    while (array[counter] != '\0') {
-        ++counter;
-    }
-    return counter;
-}
-
-void flippingAnArray(int array[], int arrayLen) {
-    for (int i = 0; i < arrayLen / 2; ++i) {
-        swap(&array[i], &array[arrayLen - 1 - i]);
-    }
-}
-
-void changingArraySegments(int array[], int lengh, int m) {
-    for (int i = 0; i < m / 2; ++i) {
-        swap(&array[i], &array[m - 1 - i]);
-    }
-    for (int i = 0; i < (lengh - m) / 2; ++i) {
-        swap(&array[m + i], &array[lengh - 1 - i]);
-    }
-    flippingAnArray(array, lengh);
-    for (int i = 0; i < lengh; ++i) {
-        printf("%d ", array[i]);
+void changingArraySegments(int array[], int firstIndex, int secondIndex) {
+    while (firstIndex <= secondIndex) {
+        swap(&array[firstIndex], &array[secondIndex]);
+        ++firstIndex;
+        --secondIndex;
     }
 }
 
 int main(void) {
-    setlocale(LC_ALL, "Rus");
-    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-    int arrayLen = lenghArray(array);
+    int array[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+    int m = 6;
+    int n = 7;
     printf("\Array:\n Output: ");
-    changingArraySegments(array, 12, 5);
-    printf("\n----------\n");
+    changingArraySegments(array, 0, m - 1);
+    changingArraySegments(array, m, m + n - 1);
+    changingArraySegments(array, 0, m + n - 1);
+    for (int i = 0; i < 13; ++i) {
+        printf("%d ", array[i]);
+    }
 }
