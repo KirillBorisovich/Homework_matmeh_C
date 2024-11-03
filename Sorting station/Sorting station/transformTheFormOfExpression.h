e7126b59dd8859ab148092a62dfebb4eb96efb3c
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-11-03 22:39:23 +0300

    feat: completed the task

--- Sorting station/Sorting station/transformTheFormOfExpression.h
+++ Sorting station/Sorting station/transformTheFormOfExpression.h
@@ -1,4 +1,10 @@
 #pragma once
 
+// Check for a number
+bool recognizeTheNumber(char element);
 
-void transformTheFormOfExpression(char* string);
+// Converting number to literal
+char meaningTranslated(int number);
+
+// Convert an expression from infix to postfix form
+char* transformTheFormOfExpression(char* string, int* errorCode);
