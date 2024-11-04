13df4b380eadab392cd7fddc0f40b971b741ae86
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-11-05 01:58:37 +0300

    feat: added a test for readingFromFile

--- homework_08_11_24/mergeSort/mergeSort/tests.c
+++ homework_08_11_24/mergeSort/mergeSort/tests.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include "list.h"
+#include "readingFromFile.h"
 #include "tests.h"
 #include "testsList.h"
 
+bool testReadingFromFile() {
+    bool result = true;
+    char* referenceValues[] = { "name3 12345", "name2 1234", "name1 123" };
+    int index = 0;
+    int errorCode = 0;
+    List* list = createList(&errorCode);
+    readingFromFile("testForReadingFromFile.txt", list, &errorCode);
+    for (Position i = first(list); !isLast(list, i); i = next(i)) {
+        char* string = getValue(list, i, &errorCode);
+        if (strcmp(string, referenceValues[index])) {
+            result = false;
+        }
+        free(string);
+        ++index;
+    }
+    deleteList(list);
+    return result && errorCode == 0;
+}
+
 bool testProgram() {
-    return testList();
+    return testList() && testReadingFromFile();
 }
 
