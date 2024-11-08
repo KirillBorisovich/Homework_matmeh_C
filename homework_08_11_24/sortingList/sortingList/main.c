616a0ac8fbd159a5b464b642f2e305ff659aa7d3
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-11-08 15:09:31 +0300

    init: started doing sortingList

---
+++ homework_08_11_24/sortingList/sortingList/main.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include "list.h"
+#include "tests.h"
+
+int main() {
+    int errorCode = 0;
+    List* list = createList(&errorCode);
+    int operationNumber = 9;
+    while (operationNumber != 0) {
+        printf("Enter operation number: ");
+        scanf_s("%d", &operationNumber);
+        getchar();
+        if (operationNumber == 0) {
+            return 0;
+        }
+        else if (operationNumber == 0) {
+            int value = 0;
+            printf("Enter value: ");
+            scanf_s("%d", &value);
+            Position position = first(list);
+            
+        }
+        else if (operationNumber == 0) {
+
+        }
+        else if (operationNumber == 0) {
+
+        }
+    }
+}
