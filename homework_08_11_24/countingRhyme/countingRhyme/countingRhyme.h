de71b3363a358e07689e93d36ff69e5a33a1b990
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-11-08 23:52:59 +0300

    refactor: added comments to functions, set arguments from the condition, made error handling

--- homework_08_11_24/countingRhyme/countingRhyme/countingRhyme.h
+++ homework_08_11_24/countingRhyme/countingRhyme/countingRhyme.h
@@ -1,3 +1,4 @@
 #pragma once
 
+// Function for counting rhyme
 void countingRhyme(List* list, int n, int m, int* errorCode);
--- homework_08_11_24/countingRhyme/countingRhyme/main.c
+++ homework_08_11_24/countingRhyme/countingRhyme/main.c
@@ -9,15 +9,21 @@ int main() {
         printf("Test failed");
         return 123;
     }
-    int n = 20;
+    int n = 41;
     int m = 3;
     int errorCode = 0;
     List* list = createList(&errorCode);
     countingRhyme(list, n, m, &errorCode);
     Position position = first(list);
-    while (next(position) != first(list)) {
-        printf("%d ", getValue(list, position));
-        position = next(position);
+    if (errorCode == 0) {
+        while (next(position) != first(list)) {
+            printf("%d ", getValue(list, position));
+            position = next(position);
+        }
     }
+    else if (errorCode == 1) {
+        printf("Memory allocation error");
+    }
+    
     deleteList(list);
 }
