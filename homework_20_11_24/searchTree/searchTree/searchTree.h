db0e2e247aafa45d456b68bc0d469abb13cad93b
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-11-13 00:44:29 +0300

    refactor: added comments to functions in searchTree.h

--- homework_20_11_24/searchTree/searchTree/searchTree.h
+++ homework_20_11_24/searchTree/searchTree/searchTree.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 typedef struct {
     int key;
@@ -7,28 +7,41 @@ typedef struct {
 
 typedef struct Node Node;
 
+// Create a node
 Node* createNode(NodeValue value, int* errorCode);
 
+// Add element to tree
 void addElementToTree(Node* node, NodeValue value, int* errorCode);
 
+// Add left child
 void addLeftChild(Node* node, Node* child);
 
+// Add right child
 void addRightChild(Node* node, Node* child);
 
+// Find element by key
 Node* findElementByKey(Node* node, int key);
 
+// Check if an element is present in a tree
 bool presenceOfElementByKey(Node* node, int key);
 
+// Get left child
 Node* getLeftChild(Node* node);
 
+// Get right child
 Node* getRightChild(Node* node);
 
+// Get value from node
 NodeValue getValue(Node* node);
 
+// Add values ​​to node
 void setValue(Node* node, NodeValue value);
 
+// Find the minimum element of the right subtree
 Node* getTheMinimumElementOfTheRightNode(Node* node);
 
+// Delete element by key
 void deleteElementByKey(Node* node, int key);
 
+// Remove tree
 void deleteTree(Node* node);
