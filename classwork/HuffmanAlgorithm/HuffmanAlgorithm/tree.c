06806df83abbe14a65626d4fa5b8bbbcdc1faa28
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-11-15 17:08:12 +0300

    feat: made an encryption algorithm

--- classwork/HuffmanAlgorithm/HuffmanAlgorithm/tree.c
+++ classwork/HuffmanAlgorithm/HuffmanAlgorithm/tree.c
@@ -5,7 +5,6 @@
 
 typedef struct Node {
     NodeValue value;
-    Node* parent;
     Node* leftChild;
     Node* rightChild;
 } Node;
@@ -21,12 +20,10 @@ Node* createNode(NodeValue value) {
 
 void addLeftChild(Node* node, Node* child) {
     node->leftChild = child;
-    child->parent = node;
 }
 
 void addRightChild(Node* node, Node* child) {
     node->rightChild = child;
-    child->parent = node;
 }
 
 Node* getLeftChild(Node* node) {
@@ -51,57 +48,6 @@ Node* getTheMinimumElementOfTheRightNode(Node* node) {
         minElement = minElement->leftChild;
     }
     return minElement;
-}
-
-void deleteElementByKey(Node* node) {
-    Node* element = node;
-    if (element != NULL) {
-        if (element->leftChild == NULL && element->rightChild == NULL) {
-            if (element->parent->leftChild == element) {
-                element->parent->leftChild = NULL;
-            }
-            else {
-                element->parent->rightChild = NULL;
-            }
-        }
-        else if (element->leftChild != NULL && element->rightChild == NULL) {
-            if (element->parent->leftChild == element) {
-                element->parent->leftChild = element->leftChild;
-            }
-            else {
-                element->parent->rightChild = element->leftChild;
-            }
-        }
-        else if (element->leftChild == NULL && element->rightChild != NULL) {
-            if (element->parent->leftChild == element) {
-                element->parent->leftChild = element->rightChild;
-            }
-            else {
-                element->parent->rightChild = element->rightChild;
-            }
-        }
-        else if (element->leftChild != NULL && element->rightChild != NULL) {
-            Node* elementReplacement = getTheMinimumElementOfTheRightNode(element);
-            if (elementReplacement->parent->leftChild == elementReplacement) {
-                elementReplacement->parent->leftChild = elementReplacement->rightChild != NULL ?
-                    elementReplacement->rightChild : NULL;
-            }
-            else {
-                elementReplacement->parent->rightChild = elementReplacement->rightChild != NULL ?
-                    elementReplacement->rightChild : NULL;
-            }
-            elementReplacement->parent = element->parent;
-            if (element->parent->leftChild == element) {
-                element->parent->leftChild = elementReplacement;
-            }
-            else {
-                element->parent->rightChild = elementReplacement;
-            }
-            elementReplacement->leftChild = element->leftChild;
-            elementReplacement->rightChild = element->rightChild;
-        }
-        free(element);
-    }
 }
 
 void deleteTree(Node* node) {
