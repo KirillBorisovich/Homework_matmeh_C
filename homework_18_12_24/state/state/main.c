33b8e40aa8a91faf9c246949d9adea89f070dfca
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-12-15 22:51:49 +0300

    feat: wrote part of the graph

--- homework_18_12_24/state/state/main.c
+++ homework_18_12_24/state/state/main.c
@@ -1,5 +1,12 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "graph.h"
+#include "testsGraph.h"
 
 int main(void) {
-
+    if (!testGraph()) {
+        printf("Test failed!\n");
+        return 123;
+    }
 }
